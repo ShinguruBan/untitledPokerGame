ec79090c5912851dea437dca446eac5197f6465d
ShinguruBan <yifandanielshan@gmail.com>
2026-08-20 14:08:33 +0200

    finish evaluation

--- untitled-poker-game/scripts/manager_hand.gd
+++ untitled-poker-game/scripts/manager_hand.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var card_database_reference
 var combination_database_reference
+var storage = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,35 +10,68 @@ func _ready() -> void:
 	combination_database_reference = preload("res://scripts/database_combination.gd")
 
 func decide_winner():
-	var current_winner
-	var current_highest_score = -1
+	var winner
+	var highest_score = -1
+	var winner_amount_sorted = []
 	
 	for player in self.get_children():
 		var amount_of_each_card = []
 		for i in range(card_database_reference.CARD_TYPES.size()):
-			amount_of_each_card.append(0)
+			amount_of_each_card.append(Vector2(0, i))		#Vector2.x = amount, Vector2.y = type_index
 		
 		for card in player.get_children():
 			for index_type in range(amount_of_each_card.size()):
 				if card.type == index_type:
-					amount_of_each_card[index_type] = amount_of_each_card[index_type] + 1
+					amount_of_each_card[index_type].x = amount_of_each_card[index_type].x + 1
 					break
 		
 		var current_score = -1
 		for amount in amount_of_each_card:
-			if amount == 2:
+			if amount.x == 2:
 				current_score = current_score + 1
-			elif amount == 3:
+			elif amount.x == 3:
 				current_score = current_score + 3
-			elif amount == 4:
+			elif amount.x == 4:
 				current_score = current_score + 5
-			elif amount == 5:
+			elif amount.x == 5:
 				current_score = current_score + 6
-		if current_score > current_highest_score:
-			current_highest_score = current_score
-			current_winner = player
-	print(current_winner.id)
-	print(combination_database_reference.POSSIBLE_COMBINATIONS[current_highest_score][combination_database_reference.INDEX_NAME])
+		if current_score > highest_score:
+			highest_score = current_score
+			amount_of_each_card.sort()
+			winner_amount_sorted.assign(amount_of_each_card)
+			winner = player
+		if current_score == highest_score:
+			amount_of_each_card.sort()
+			for i in range(winner_amount_sorted.size() - 1, 0, -1):
+				if amount_of_each_card[i].y > winner_amount_sorted[i].y:
+					winner_amount_sorted.assign(amount_of_each_card)
+					winner = player
+	print(winner.id)
+	print(combination_database_reference.POSSIBLE_COMBINATIONS[highest_score][combination_database_reference.INDEX_NAME])
+	print(winner_amount_sorted)
+
+func sort_amount_of_each_card(amount_of_each_card):
+	mergesort(amount_of_each_card, 0, amount_of_each_card.size() - 1)
+
+func mergesort(array, left, right):
+	var middle = (left + right) / 2
+	if left < right:
+		mergesort(array, left, middle)
+		mergesort(array, middle + 1, right)
+		merge(array, left, middle, right)
+
+func merge(array, left, middle, right):
+	var left_index = left
+	var right_index = right
+	for i in range(left, right + 1):
+		if right_index > right || (left_index <= middle && array[left_index] <= right_index):
+			storage[i] = array[left_index]
+			left_index = left_index + 1
+		else:
+			storage[i] = array[right_index]
+			right_index = right_index + 1
+	for i in range(left, right + 1):
+		array[i] = storage[i]
 
 func deal_new_cards():
 	for player in self. get_children():
